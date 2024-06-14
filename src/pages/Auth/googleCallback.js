9602525ee00ca6fb2c3a052b7922ea2aea9dfafd
Allan Juma <allancjuma@gmail.com>
2024-06-14 03:14:04 +0300

    update:  add correct useeffect

--- src/pages/Auth/googleCallback.js
+++ src/pages/Auth/googleCallback.js
@@ -1,35 +1,36 @@
 import useUserData from "Hooks/useUserData";
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { useLocation, useNavigate } from "react-router-dom";
-import { apiCall } from "redux/async/asyncThunk";
-import {
-  loginFailed,
-  loginSuccess,
-  selectUserError,
-  selectUserStatus,
-} from "redux/features/user/userSlice";
+import { loginFailed, loginSuccess } from "redux/features/user/userSlice";
 
 const GoogleCallback = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const userStatus = useSelector(selectUserStatus);
   const userData = useUserData();
   const { search } = useLocation();
   const params = new URLSearchParams(search);
   const userId = params.get("userId");
-  if (!userId) {
-    dispatch(loginFailed({ error: "No user ID" }));
-    navigate("/login");
-  }
-  const userPayload = {
-    ...userData,
-    userId,
-  };
-  console.log(userPayload);
 
-  dispatch(loginSuccess(userPayload));
-  return navigate("/dashboard");
+  useEffect(() => {
+    if (!userId) {
+      dispatch(
+        loginFailed({ error: "No user ID found in the query parameters." })
+      );
+      navigate("/login");
+      return;
+    }
+
+    const userPayload = {
+      ...userData,
+      userId,
+    };
+
+    dispatch(loginSuccess(userPayload));
+    navigate("/dashboard");
+  }, [dispatch, navigate, userId, userData]);
+
+  return null;
 };
 
 export default GoogleCallback;
